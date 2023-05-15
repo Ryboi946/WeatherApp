b6d7cb02224adfd9e090dc83b0f0568c5c476c1b
unknown <rcurry2@ycp.edu>
2023-05-14 23:13:59 -0400

    Second Commit

--- src/Current.js
+++ src/Current.js
@@ -4,14 +4,15 @@ divItem.addEventListener('click', CurrentWeather);
 
 let API_Key = 'fcdad555357675fa31f08e394ede826d';
 let country_code = 'US';
-
+let GeoURL = null;
 
 function CurrentWeather() {
     
-    let zipcode = document.getElementById('enter').value;
-    let URL = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}'
-    let GeoURL = `http://api.openweathermap.org/geo/1.0/zip?zip=${zipcode},${country_code}&appid=${API_Key}`;
-    GetCoordinates(GeoURL, zipcode);
+    let zipcode = document.getElementById('search').value;
+    
+    GeoURL = `http://api.openweathermap.org/geo/1.0/zip?zip=${zipcode},${country_code}&appid=${API_Key}`;
+    console.log(zipcode);
+    GetCoordinates(GeoURL);
     
 }
 
@@ -20,17 +21,25 @@ function CurrentWeather() {
 async function GetCoordinates(GeoURL) {
     const response = await fetch(GeoURL);
     const data = await response.json();
-    lat = data.lat;
-    lon = data.lon;
+    console.log(data);
+    let lat = data.lat;
+    let lon = data.lon;
     console.log(lat);
     console.log(lon);
-    WeatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_Key}`;
+    let WeatherURL = `https://api.openweathermap.org/data/2.5/weather?units=imperial&lat=${lat}&lon=${lon}&appid=${API_Key}`;
     
     GetWeather(WeatherURL);
   }
 
   async function GetWeather(WeatherURL) {
     const response = await fetch(WeatherURL);
-    const data = response.json();
+    const data = await response.json();
+    console.log(data);
+    let temp = data.main.temp;
+
+    let p = document.createElement('p');
+    p.textContent = temp;
+
+    document.body.appendChild(p);
 
   }
